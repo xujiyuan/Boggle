65a4f396094aa3b27af8c1535cbacaa6e5449a2d
Xu, Jimmy <xuj1@ohio.edu>
2019-05-10 09:16:19 -0400

    move wrapper to beforeEach

--- tests/unit/boggle.spec.js
+++ tests/unit/boggle.spec.js
@@ -1,29 +1,34 @@
-import { expect } from 'chai'
-import { shallowMount } from '@vue/test-utils'
+import {expect} from 'chai'
+import {shallowMount} from '@vue/test-utils'
 import Boggle from '@/components/Boggle.vue';
 
 describe('Boggle.vue', () => {
+    let wrapper;
 
-  it('test method is called', () => {
-    const wrapper = shallowMount(Boggle);
-    const result = wrapper.vm.test();
-    expect(result).equals('test');
-  });
+    beforeEach(() => {
+        wrapper = shallowMount(Boggle);
+    });
 
-  it('parseInput should covert string into 1D list', () => {
-    expect(1).equals(2);
-  });
+    it('test method is called', () => {
+        const result = wrapper.vm.test();
+        expect(result).equals('test');
+    });
 
-  it('covertTo2D should take a list and covert it to 2d array', () => {
-    expect(1).equals(2);
-  });
+    it('parseInput should covert string into 1D list', () => {
 
-  it('coverCharToNumber should change a char to a number for calculation purpose', () => {
-    expect(1).equals(2);
-  });
+        expect(1).equals(2);
+    });
 
-  it('findWord should find possible words', () => {
-    expect(1).equals(2);
-  });
+    it('covertTo2D should take a list and covert it to 2d array', () => {
+        expect(1).equals(2);
+    });
+
+    it('coverCharToNumber should change a char to a number for calculation purpose', () => {
+        expect(1).equals(2);
+    });
+
+    it('findWord should find possible words', () => {
+        expect(1).equals(2);
+    });
 
 });
