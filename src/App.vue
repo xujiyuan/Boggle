9ebc364b4221bc21cbbf83080cee01ac3ad4aec1
Xu, Jimmy <xuj1@ohio.edu>
2019-05-10 09:03:11 -0400

    Built a simple vue project as container for this task. Start the development at 9:03 est

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://66.media.tumblr.com/17352e8406b0587ea62d7d101bcf8090/tumblr_orvo5zynew1rdod18o1_400.gif">
+    <Boggle msg="Bad Boggle Game @Beta 0.0.1" something = 'HHA'/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Boggle from './components/Boggle.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'app',
+    components: {
+      Boggle,
+    },
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
+
---
+++ src/components/Boggle.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h2>"Bad Boggle" Instructions</h2>
+        <pre>
+      Example Input:
+      urbx
+      oygj
+      fuya
+
+      Example Output:
+      Word: "uofu", Score: 7
+    </pre>
+
+        <form id="mazeForm">
+            <label for="mazeEntry">Please input your grid and click "Process"</label><br/>
+            <textarea name="mazeEntry" id="mazeEntry" cols="30" rows="10"></textarea>
+            <br/>
+            <input id="mazeEntrySubmit" type="submit" value="Process">
+            <div id="mazeSolution"></div>
+        </form>
+    </div>
+
+</template>
+<script>
+    export default {
+        name: 'Boggle'
+    }
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
+</style>
